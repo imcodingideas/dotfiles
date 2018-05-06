20d8f5270fd7cfe3cfa37016031a5f965fe37562
Joseph Chambers <joseph@Josephs-iMac.local>
2018-05-05 18:19:50 -0700

    Updated scripts on fresh install.

---
+++ scripts.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+easy_install pip
+
+brew install gpg
+brew install wget
+brew install tree
+brew install yarn
+brew install htop
+brew install z
---
+++ software.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# homebrew-cask
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+# browsers
+brew cask install google-chrome
+
+# coding
+brew cask install iterm2
+brew cask install visual-studio-code
+
+# essential
+brew cask install alfred
+brew cask install dropbox
+brew cask install google-drive-file-stream
+
+# for fun
+brew cask install spotify
+brew cask install vlc
+
+# tools
+brew cask install sketch
+
+# Communication
+brew cask install skype
+brew cask install slack
+brew cask install discord
+brew cask install the-unarchiver
